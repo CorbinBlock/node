fea97af6df77d9d875e3ff5ce77c9a02854f4033
CorbinBlock <williamcorbinblock@gmail.com>
2022-03-15 15:34:28 -0500

    javascript function - execute powershell

--- myapp/execute_shell.js
+++ myapp/execute_shell.js
@@ -1,19 +1,17 @@
-module.exports = {
-    execShellCommand : function() {
-        const { exec } = require("child_process");
-        // String will be executed in Command Prompt
-        const cmd = 'pwsh.exe -c "Stop-Programs"';
-    
-        exec(cmd, (error, stdout, stderr) => {
-            if (error) {
-                console.log(`error: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-                return;
-            }
-            console.log(`stdout: ${stdout}`);
-        })
-    }
-  };
+export function execShellCommand() {
+    const { exec } = require("child_process");
+    // String will be executed in Command Prompt
+    const cmd = 'pwsh.exe -c "Stop-Programs"';
+
+    exec(cmd, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+}
