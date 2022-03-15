46f065f799b71b5244011ac070f928cc521dbe69
corbinblock <williamcorbinblock@gmail.com>
2022-03-14 20:53:59 -0500

    nodejs project- express app

---
+++ myapp/execute_shell.js
@@ -0,0 +1,16 @@
+const cmd = 'pwsh.exe -c "Get-LastBootTime"'
+function execShellCommand(cmd) {
+    const { exec } = require("child_process");
+    exec(cmd, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    })
+}
+execShellCommand(cmd)
