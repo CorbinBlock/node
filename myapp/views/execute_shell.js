05bbd14474e0a87283ee901e0bab7a4508e49809
CorbinBlock <williamcorbinblock@gmail.com>
2022-03-15 12:09:15 -0500

    attempting to call js function through pug template

---
+++ myapp/views/execute_shell.js
@@ -0,0 +1,16 @@
+function execShellCommand(cmd) {
+    const { exec } = require("child_process");
+    exec(cmd, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    })
+}
+
+// execShellCommand('pwsh.exe -c "Get-Creds admin; Enter-RDP"')
